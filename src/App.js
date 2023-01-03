6e4acf7f326f259aafcde6ddb7eeb082176204f1
iambhautik <iambhautikgajera@gmail.com>
2023-01-03 18:13:12 +0530

    context API demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { Fragment } from 'react';
+import { Component, Fragment } from 'react';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
+import { ThemeContextProvider } from './context/themeContext';
 
 function App() {
+
+
+  const styles = {
+    'backgroundColor': 'skyblue'
+  }
+
+
   return (
     <Fragment>
-      <p>Hello MobX</p>
+      <ThemeContextProvider value={styles} >
+        <Component1 />
+        <Component2 />
+      </ThemeContextProvider>
+        <p>Hello MobX</p>
     </Fragment>
   );
 }
